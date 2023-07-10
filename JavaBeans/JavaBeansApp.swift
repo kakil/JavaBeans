d2ae9886ebba7d7f4bf2d8adef4d4e7cafc9448f
Kitwana Akil <kakil@me.com>
2023-07-10 19:50:01 -0400

    Initial Commit

---
+++ JavaBeans/JavaBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  JavaBeansApp.swift
+//  JavaBeans
+//
+//  Created by Kitwana Akil on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct JavaBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
